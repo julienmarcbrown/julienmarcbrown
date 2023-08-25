912b94b56044546ed3bae835a41cc7d3560c02c1
Julien Marc Brown <mail@julienmarcbrown.com>
2023-08-24 19:48:55 -0700

    update

---
+++ components/biography.tsx
@@ -0,0 +1,13 @@
+
+const Biography = () => {
+  return (
+    <section className="w-8/12 border-gray-500">
+        <p className="font-light pt-12">
+            Hi! Welcome to my personal site. I am a software engineer, engineering leader and entrepreneur!
+            I don't really post here that often, but poke around!
+        </p>
+    </section>
+  )
+}
+
+export default Biography
---
+++ components/social-links.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import Container from "./container";
+
+const SocialLinks = () => {
+    return (
+        <section className="flex">
+            <Link href="mailto:mail@julienmarcbrown/"><Image
+                src="/social-icons/mail.svg"
+                height={32}
+                width={32}
+                alt="Email me"
+            /></Link>
+            <Link href="https://www.linkedin.com/in/julienmarcbrown/"><Image
+                src="/social-icons/linkedin.svg"
+                height={32}
+                width={32}
+                alt="Follow me on LinkedIn"
+            /></Link>
+            <Link href="https://www.github.com/julienmarcbrown/"><Image
+                src="/social-icons/github.svg"
+                height={32}
+                width={32}
+                alt="Follow me on Github"
+            /></Link>
+        </section>
+    )
+}
+
+export default SocialLinks
