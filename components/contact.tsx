689a8c16e8ab3bb4c36e7a1a16e8ddaaabc8d80d
Julien Marc Brown <mail@julienmarcbrown.com>
2023-09-03 16:27:35 -0700

    Update contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,10 +1,17 @@
+import {CalendarIcon} from "@heroicons/react/solid";
+import {MailIcon} from "@heroicons/react/solid";
 
+import Link from "next/link";
+
+CalendarIcon
 export default function Contact() {
   return (
     <div className="p-24">
-        <h2 className="font-semibold text-3xl text-center p-6" id="about">About Me</h2>
-            <div className="w-full pb-6 mx-auto">
-                <div>Call me
+        <h2 className="font-semibold text-3xl text-center p-6" id="about">Contact Me</h2>
+            <div className="w-full mx-auto">
+                <div className="flex text-center font-semibold text-lg space-x-12">
+                    <Link href="https://calendly.com/julienmarcbrown"><CalendarIcon className="h-6 w-6"/>Meet</Link>
+                    <Link href="mailto:hire@julienmarcbrown.com"><MailIcon className="h-6 w-6"/>Email</Link>
                 </div>
             </div>
     </div>
