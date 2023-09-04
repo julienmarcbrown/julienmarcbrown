73815a7e0acabbed3da991f77b147d28e73fd61f
Julien Marc Brown <mail@julienmarcbrown.com>
2023-09-03 19:37:21 -0700

    Add GAScript.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import GAScript from "./GAScript";
 
 const Meta = () => {
   return (
@@ -37,6 +38,7 @@ const Meta = () => {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+    <GAScript/>
     </Head>
   )
 }
